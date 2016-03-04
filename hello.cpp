644b29af2646bd2353421c6d61272b0a75d98ab1
Chuckeles <martin@jakubik.info>
2016-03-04 21:59:39 +0100

    Add ugly code

--- hello.cpp
+++ hello.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
+ #include  <string>
 
-int main() { std::cout << "Hello World"; }
+int main()
+
+{
+    std::string name ( "chuckeles");
+    
+   std::cout << "Hello World" <<     std::endl;
+  
+  
+std::cout << "I am " << name   <<
+"and this code used to be ugly..." << std::endl;
+
+return 0;
+
+  }
